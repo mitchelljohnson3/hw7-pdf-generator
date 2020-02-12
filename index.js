117998ec0194e760ba8735ad6c455cdc1b7fd3ec
Mitchell-Johnson <mitchj9@uw.edu>
2020-02-12 00:54:28 -0800

    MHMM

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const inq = require ('inquirer');
 const convertFactory = require ('electron-html-to');
+const axios = require('axios');
 const fs = require('fs');
 
 // sets the conversion type target to be PDF
@@ -10,38 +11,36 @@ const conversion = convertFactory({
 // this function will take the github username, and fill the index.html file with the data
 function fetchData(answers) {
     const url = `https://api.github.com/users/${answers.username}`
-    // gotta use axios buddy
-    fetch(url)
-        .then( (response) => {
-            return response.json();
-        })
+    // fetches data from url
+    axios.get(url)
         .then( (json) => {
-            return fillData(json, answers.color);            
+            fillData(json.data, answers.color);
         })
 }
 
 function fillData(json, userColor) {
     // fills index.html file with data
+    const location = json.location.split(' ').join('+');
     const template = `<!DOCTYPE html>
     <html lang="en">
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
+        <title>User Profile PDF</title>
     </head>
     <body>
-        <img src="${imageUrl}">
+        <img src="${json.avatar_url}">
         <h1>${json.login}</h1>
         <ul>
-            <li><a href="${userLocationUrl}">${userLocationUrl}</a></li>
+            <li><a href="https://www.google.com/maps/place/${location}">https://www.google.com/maps/place/${location}</a></li>
             <li><a href="${json.html_url}">${json.html_url}</a></li>
-            <li><a href="${json.blog}">${json.blog}</a></li>
+            <li><a href="https://${json.blog}">https://${json.blog}</a></li>
         </ul>
         <h2>${json.bio}</h2>
-        <h3>${numRepos} repositories</h3>
-        <h3>${numRepos} followers</h3>
-        <h3>${numRepos} github stars</h3>
-        <h3>following ${numRepos} users</h3>
+        <h3>${json.public_repos} repositories</h3>
+        <h3>${json.followers} followers</h3>
+        <h3>${json.public_gists} github stars</h3>
+        <h3>following ${json.following} users</h3>
     
         <style>
             body{
@@ -50,22 +49,16 @@ function fillData(json, userColor) {
         </style>
     </body>
     </html>`;
-
-    return template;
+    convert(template);
 }
 
 function convert(htmlFile) {
     // converts html file to pdf
-    conversion({ html: htmlFile }, function(err, result) {
-        if (err) {
-            return console.error(err);
+    fs.writeFile('profile.html', htmlFile, (err) => {
+        if(err){
+            throw err;
         }
-        
-        console.log(result.numberOfPages);
-        console.log(result.logs);
-        result.stream.pipe(fs.createWriteStream('/path/to/anywhere.pdf'));
-        conversion.kill(); // necessary if you use the electron-server strategy, see bellow for details
-    });
+    })
 }
 
 const init = async function(questions) {
@@ -73,14 +66,14 @@ const init = async function(questions) {
 };
 
 inq.prompt(
-    {
+    [{
         message: 'github username?',
         name: 'username'
     },
     {
         message: 'favorite color?',
         name: 'color'
-    }
+    }]
 ).then( (questions) => {
     // this will initialize the program
     init(questions);
